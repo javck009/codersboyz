91e06cb703b53cb64d36adc58629ce6157022aed
javck009 <84823936+javck009@users.noreply.github.com>
2021-06-17 09:46:29 +0530

    Code

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+
+
+function open_click(){
+  document.getElementById('right-slide').style.display='block';
+}
+function close_click(){
+  document.getElementById('right-slide').style.display='none';
+}
